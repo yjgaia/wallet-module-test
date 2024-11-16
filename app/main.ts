a48000717588c44da52771bbb33ad3b99858491c
yjgaia <yjdev88@gmail.com>
2024-11-16 19:31:00 +0900

    Update dependencies and refactor main.ts to integrate WalletLoginManager and SupabaseConnector

--- app/main.ts
+++ app/main.ts
@@ -1,13 +1,10 @@
 import { BodyNode } from "@common-module/app";
 import { Button, ButtonType } from "@common-module/app-components";
-import { WalletSessionManager } from "@common-module/wallet";
+import { SupabaseConnector } from "@common-module/supabase";
 import {
-  createSIWEConfig,
-  formatMessage,
-  type SIWECreateMessageArgs,
-  type SIWESession,
-  type SIWEVerifyMessageArgs,
-} from "@reown/appkit-siwe";
+  WalletLoginConfig,
+  WalletLoginManager,
+} from "@common-module/wallet-login";
 import { mainnet } from "@reown/appkit/networks";
 import ParsingNFTDataABI from "./ParsingNFTData.json" with {
   type: "json",
@@ -22,95 +19,38 @@ const metadata = {
   icons: ["https://avatars.githubusercontent.com/u/179229932"],
 };
 
-async function getSession() {
-  const res = await fetch(BASE_URL + "/session", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    credentials: "include",
-  });
-  if (!res.ok) {
-    throw new Error("Network response was not ok");
-  }
-
-  const data = await res.json();
-  return data == "{}" ? null : data as SIWESession;
-}
-
-const verifyMessage = async ({ message, signature }: SIWEVerifyMessageArgs) => {
-  try {
-    const response = await fetch(BASE_URL + "/verify", {
-      method: "POST",
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-      },
-      mode: "cors",
-      body: JSON.stringify({ message, signature }),
-      credentials: "include",
-    });
-
-    if (!response.ok) {
-      return false;
-    }
-
-    const result = await response.json();
-    return result === true;
-  } catch (error) {
-    return false;
-  }
-};
-
-const siweConfig = createSIWEConfig({
-  getMessageParams: async () => ({
-    domain: window.location.host,
-    uri: window.location.origin,
-    chains: [1, 2020],
-    statement: "Please sign with your account",
-  }),
-  createMessage: ({ address, ...args }: SIWECreateMessageArgs) =>
-    formatMessage(args, address),
-
-  getNonce: async () => { //This is only an example, substitute it with your actual nonce getter.
-    const nonce =
-      "0f3c11af88662c90529dc2b8382ad56c507c06d64c9c365144e0937f00a11359";
-    if (!nonce) {
-      throw new Error("Failed to get nonce!");
-    }
-    return nonce;
-  },
-  getSession,
-  verifyMessage,
-  signOut: async () => { //Example
-    // Implement your Sign out function
-    return true;
-  },
-});
-
-const BASE_URL = "http://localhost:8080";
-
 (async () => {
   console.log("Start Test");
 
-  WalletSessionManager.init({
+  WalletLoginConfig.supabaseConnector = new SupabaseConnector(
+    "https://dhzxulywizygtdficytt.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRoenh1bHl3aXp5Z3RkZmljeXR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAxMTIxNDUsImV4cCI6MjA0NTY4ODE0NX0.xUd8nqcT2aVn1j4x8c-pRbDcFSaIGtkn7SAcmKleBms",
+    WalletLoginManager,
+  );
+
+  WalletLoginManager.init({
     projectId,
     metadata,
     networks: [mainnet],
-    //siweConfig,
   });
 
-  WalletSessionManager.on("sessionChanged", (walletAddress) => {
+  /*WalletLoginManager.on("sessionChanged", (walletAddress) => {
     console.log("Wallet Address:", walletAddress);
-  });
+  });*/
 
   new Button({
     type: ButtonType.Contained,
     title: "Open Modal",
-    onClick: () => WalletSessionManager.openModal(),
+    onClick: () => WalletLoginManager.openWallet(),
   }).appendTo(BodyNode);
 
-  const result = await WalletSessionManager.readContract({
+  new Button({
+    type: ButtonType.Contained,
+    title: "Disconnect",
+    onClick: () => WalletLoginManager.disconnect(),
+  }).appendTo(BodyNode);
+
+  const result = await WalletLoginManager.readContract({
     chainId: 1,
     address: "0x06f98E2E91E64103d612243a151750d14e5EDacC",
     abi: ParsingNFTDataABI.abi,
