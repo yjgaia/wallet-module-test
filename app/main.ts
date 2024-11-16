88cf20d3f0281eebda744eb8cdf09892d2bcfba2
yjgaia <yjdev88@gmail.com>
2024-11-16 22:21:50 +0900

    Update dependencies and refactor main.ts to integrate new app components and session management

--- app/main.ts
+++ app/main.ts
@@ -1,14 +1,21 @@
 import { BodyNode } from "@common-module/app";
 import { Button, ButtonType } from "@common-module/app-components";
-import { SupabaseConnector } from "@common-module/supabase";
+import { createAppKit } from "@reown/appkit";
+import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
 import {
-  WalletLoginConfig,
-  WalletLoginManager,
-} from "@common-module/wallet-login";
+  createSIWEConfig,
+  formatMessage,
+  type SIWECreateMessageArgs,
+  type SIWESession,
+  type SIWEVerifyMessageArgs,
+} from "@reown/appkit-siwe";
 import { mainnet } from "@reown/appkit/networks";
+import { createPublicClient, http } from "viem";
 import ParsingNFTDataABI from "./ParsingNFTData.json" with {
   type: "json",
 };
+import { SupabaseConnector } from "@common-module/supabase";
+import { WalletLoginConfig, WalletLoginManager } from "@common-module/wallet-login";
 
 const projectId = "7538ca3cec20504b06a3338d0e53b028";
 
@@ -34,9 +41,9 @@ const metadata = {
     networks: [mainnet],
   });
 
-  /*WalletLoginManager.on("sessionChanged", (walletAddress) => {
-    console.log("Wallet Address:", walletAddress);
-  });*/
+  //WalletLoginManager.on("sessionChanged", (walletAddress) => {
+  //  console.log("Wallet Address:", walletAddress);
+  //});
 
   new Button({
     type: ButtonType.Contained,
@@ -46,8 +53,8 @@ const metadata = {
 
   new Button({
     type: ButtonType.Contained,
-    title: "Disconnect",
-    onClick: () => WalletLoginManager.disconnect(),
+    title: "Logout",
+    onClick: () => WalletLoginManager.logout(),
   }).appendTo(BodyNode);
 
   const result = await WalletLoginManager.readContract({
@@ -63,3 +70,135 @@ const metadata = {
 
   console.log("Result:", result);
 })();
+
+/*export const networks: any = [mainnet];
+
+// 2. Set up Wagmi adapter
+const wagmiAdapter = new WagmiAdapter({
+  projectId,
+  networks,
+});
+
+async function getSession() {
+  const res = await fetch(BASE_URL + "/session", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  });
+  if (!res.ok) {
+    throw new Error("Network response was not ok");
+  }
+
+  const data = await res.json();
+  return data == "{}" ? null : data as SIWESession;
+}
+
+const verifyMessage = async ({ message, signature }: SIWEVerifyMessageArgs) => {
+  try {
+    const response = await fetch(BASE_URL + "/verify", {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+      mode: "cors",
+      body: JSON.stringify({ message, signature }),
+      credentials: "include",
+    });
+
+    if (!response.ok) {
+      return false;
+    }
+
+    const result = await response.json();
+    return result === true;
+  } catch (error) {
+    return false;
+  }
+};
+
+const siweConfig = createSIWEConfig({
+  getMessageParams: async () => ({
+    domain: window.location.host,
+    uri: window.location.origin,
+    chains: [1, 2020],
+    statement: "Please sign with your account",
+  }),
+  createMessage: ({ address, ...args }: SIWECreateMessageArgs) =>
+    formatMessage(args, address),
+  getNonce: () =>
+    WalletLoginConfig.supabaseConnector.callEdgeFunction<string>(
+      "siwe/nonce",
+    ),
+  verifyMessage: async ({ message, signature }) => {
+    const token = await WalletLoginConfig.supabaseConnector
+      .callEdgeFunction<string>(
+        "siwe/verify",
+        {
+          message,
+          signature,
+          projectId: projectId,
+        },
+      );
+    WalletLoginManager.token = token;
+    return true;
+  },
+  getSession: async () => {
+    const result = await WalletLoginConfig.supabaseConnector
+      .callEdgeFunction<{ address: string; chainId: number }>(
+        "siwe/session",
+      );
+    return (result.address && result.chainId) ? result : null;
+  },
+  signOut: async () => true,
+});
+
+// 3. Create the modal
+const modal = createAppKit({
+  adapters: [wagmiAdapter],
+  networks: [mainnet],
+  metadata,
+  projectId,
+  features: {
+    analytics: true, // Optional - defaults to your Cloud configuration
+  },
+  siweConfig: siweConfig,
+});
+
+const BASE_URL = "http://localhost:8080";
+
+(async () => {
+  console.log("Start Test");
+
+  WalletLoginConfig.supabaseConnector = new SupabaseConnector(
+    "https://dhzxulywizygtdficytt.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRoenh1bHl3aXp5Z3RkZmljeXR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAxMTIxNDUsImV4cCI6MjA0NTY4ODE0NX0.xUd8nqcT2aVn1j4x8c-pRbDcFSaIGtkn7SAcmKleBms",
+    WalletLoginManager,
+  );
+
+  new Button({
+    type: ButtonType.Contained,
+    title: "Open Modal",
+    onClick: () => modal.open(),
+  }).appendTo(BodyNode);
+
+  const client = createPublicClient({
+    chain: mainnet as any,
+    transport: http(),
+  });
+
+  const result = await client.readContract({
+    address: "0x06f98E2E91E64103d612243a151750d14e5EDacC",
+    abi: ParsingNFTDataABI.abi,
+    functionName: "getERC721BalanceList_OneToken",
+    args: [
+      "0x134590ACB661Da2B318BcdE6b39eF5cF8208E372",
+      ["0xbB22b6F3CE72A5Beb3CC400d9b6AF808A18E0D4c"],
+    ],
+  });
+
+  console.log("Result:", result);
+})();
+*/
---
+++ docs/_6d4d-_7391-_028a.js
@@ -0,0 +1,41 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["_6d4d-_7391-_028a"],{
+
+/***/ "?6d4d":
+/*!************************!*\
+  !*** buffer (ignored) ***!
+  \************************/
+/***/ (() => {
+
+eval("/* (ignored) */\n\n//# sourceURL=webpack:///buffer_(ignored)?");
+
+/***/ }),
+
+/***/ "?7391":
+/*!**********************!*\
+  !*** util (ignored) ***!
+  \**********************/
+/***/ (() => {
+
+eval("/* (ignored) */\n\n//# sourceURL=webpack:///util_(ignored)?");
+
+/***/ }),
+
+/***/ "?028a":
+/*!**********************!*\
+  !*** util (ignored) ***!
+  \**********************/
+/***/ (() => {
+
+eval("/* (ignored) */\n\n//# sourceURL=webpack:///util_(ignored)?");
+
+/***/ })
+
+}]);
